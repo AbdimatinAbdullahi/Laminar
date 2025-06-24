3b36c0e4ff12267b66016b43b6ef9150d0342700
Abdimatin Abdullahi <abdimatmicrosoft@gmail.com>
2025-06-24 15:45:42 +0300

    Delete workspace UI done

---
+++ frontend/src/modals/DeleteWorkspace.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import style from '../Styles/modals.module.css'
+import { X } from 'lucide-react'
+
+function DeleteWorkspace({ onClose }) {
+  return (
+    <div className={style.modalOverlay} >
+        <div className={style.deleteModalContainer}>
+          <X className={style.Icon} size={57} onClick={onClose} />
+            <div className={style.deleteTextContent}>
+              <h2>Only the workspace creator can delete this workspace.</h2>
+              <p>This action is permanent and will remove all messages, members, and data.</p>
+              <button>Are you sure you want to continue?</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default DeleteWorkspace
