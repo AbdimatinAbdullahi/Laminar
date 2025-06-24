c7f62f55875b8d9e9b4e9c20a56b73f0492e229b
Abdimatin Abdullahi <abdimatmicrosoft@gmail.com>
2025-06-24 15:57:34 +0300

    Leave workspace UI done

---
+++ frontend/src/modals/Leaveworkspace.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import style from '../Styles/modals.module.css'
+
+function Leaveworkspace({onClose}) {
+  return (
+    <div className={style.modalOverlay}>
+
+        <div className={style.leaveContainer}>
+
+            <div className={style.LeaveWorskpaceTexts}>
+                <h2>Are you sure you want to leave Acme Inc?</h2>
+                <p>You’ll lose access to all channels, messages, and files in this workspace.</p>
+                <span>This action is immediate and cannot be undone, until the admin readds you to workspace</span>
+            </div>
+            
+            <div className={style.leaveButton}>
+                <button>Leave workspace</button>
+                <button onClick={onClose} style={{backgroundColor: "#5a67d8"}} >Cancel</button>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Leaveworkspace
