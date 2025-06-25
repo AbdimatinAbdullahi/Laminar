62d975c4871195f91fe5004b07597c28a77dbf24
Abdimatin Abdullahi <abdimatmicrosoft@gmail.com>
2025-06-25 21:11:42 +0300

    Chat window

--- frontend/src/components/ChatWindow.jsx
+++ frontend/src/components/ChatWindow.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import style from '../Styles/chatwindow.module.css'
 
+
+import ChannelHeader from './ChannelHeader'
+import Conversation from './Conversation'
+import ChatComposer from './ChatComposer'
+
 function Chatwindow() {
+
+  const [showUsersBar, setshowUsersBar] = useState(false)
+
+
   return (
     <div className={style.chatContainer}>
-      Chat Window
+      <ChannelHeader/>
+      <Conversation/>
+      <ChatComposer/>
     </div>
   )
 }
