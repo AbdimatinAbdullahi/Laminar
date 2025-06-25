cf7bf223c160a8287f7d073e2ee888c96c0f1235
Abdimatin Abdullahi <abdimatmicrosoft@gmail.com>
2025-06-25 21:10:26 +0300

    Container to display the messages

---
+++ frontend/src/components/Conversation.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import style from '../Styles/chatwindow.module.css'
+
+
+function Conversation() {
+  return (
+    <div className={style.convertsationContainer}>
+      <h1>Hello world!</h1>
+    </div>
+  )
+}
+
+export default Conversation
