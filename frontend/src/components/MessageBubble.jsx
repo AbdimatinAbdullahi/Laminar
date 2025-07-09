36257d0a7cb8e1ab454d823646929a5318e2e24c
Abdimatin Abdullahi <abdimatmicrosoft@gmail.com>
2025-07-09 09:08:49 +0300

    Message Reaction picker and reply to UI

--- frontend/src/components/MessageBubble.jsx
+++ frontend/src/components/MessageBubble.jsx
@@ -1,14 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import style from '../Styles/chatroom.module.css'
+import { Plus, Reply } from 'lucide-react'
+import EmojiPicker, { Emoji } from 'emoji-picker-react'
+import {useChat} from '../context/ChatContext'
+
+
 
 function MessageBubble({message}) {
+  
+  const {state} = useChat()
+  const {messages} = state
+  const [hoverOver, sethoverOver] = useState(false)
+  const [showPicker, setshowPicker] = useState(false)
+  const pickerRef = useRef(null)
+
+  useEffect(()=>{
+    const handleOutsideClick = (event) =>{
+      if(pickerRef.current && !pickerRef.current.contains(event.target)){
+        setshowPicker(false)
+      }
+    }
+
+    document.addEventListener("mousedown", handleOutsideClick) //Binding event listre to whole document
+    return ()=> document.removeEventListener("mousedown", handleOutsideClick)
+  }, [])
+
+
+
+  const parentMessage = message.thread_parent_id ? messages.find((m) => m.id === message.thread_parent_id) : null
+
+
+
   return (
-    <div className={style.messageBubble}>
+    <div className={style.messageBubble} onMouseEnter={()=>sethoverOver(true)} onMouseLeave={()=>sethoverOver(false)} >
        
+       {showPicker && <div className={style.emojiPicker} ref={pickerRef} > <EmojiPicker onEmojiClick={()=>setshowPicker(false)}  /> </div>}
+
+       {hoverOver && <div className={style.reactionPicker}> 
+                    <button>✅</button> 
+                    <button>👀</button>  
+                    <button>👍</button>
+                    <Plus className={style.openReactionPicker} onClick={()=>setshowPicker(true)}  />  
+                    <Reply className={style.messageReply} />
+                    </div>}
+
+        {parentMessage && <div className={style.replyTo}> @{} </div>}
        <div className={style.avatarURL}>{message?.Sender.fullname.slice(0, 1).toUpperCase()}</div>
        
+       {/* Message Content => Sender Name => Message Timestamp => message Reaction */}
        <div className={style.messageContent}>
-
         <div className={style.senderItems}>
           <div className={style.senderName}>{message?.Sender.fullname}</div>
           <div className={style.sendTimestamp}>{new Date(message?.timestamp).toLocaleDateString("en-US", {day: "2-digit", month:"2-digit", year:"numeric", hour: "2-digit", minute:"2-digit", hour12: true})}</div>
